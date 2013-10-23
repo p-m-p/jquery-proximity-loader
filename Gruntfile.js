950140752bfa2d2b031e51638563e4257033e607
Phil Parsons <phil.m.parsons@gmail.com>
2013-10-23 20:50:34 +0100

    Add uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,9 +13,19 @@ module.exports = function (grunt) {
             , specs: 'spec/**/*-spec.js'
           }
       }
+    , uglify: {
+        plugin: {
+          // FIXME add version
+          files: {
+            'js/jquery-proximity-loader.min.js': 'js/jquery-proximity-loader.js'
+          }
+        }
+      }
   });
 
   grunt.registerTask('test', ['jshint', 'jasmine']);
+  grunt.registerTask('default', ['test', 'uglify']);
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 };
